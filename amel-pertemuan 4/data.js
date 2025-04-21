16dfbf81cf62ec3aa7fad52655adf06b2ddbbf20
ameliaamirah <ameliaamirah04@gmail.com>
2025-04-22 05:05:29 +0700

    Add files via upload

---
+++ amel-pertemuan 4/data.js
@@ -0,0 +1,32 @@
+[
+    {
+      "id": 1,
+      "name": "Johnsone Doe",
+      "email": "johnsone.doe@example.com",
+      "age": 32
+    },
+    {
+      "id": 2,
+      "name": "Jhonny.doe",
+      "email": "Jhonny.doe@example.com",
+      "age": 28
+    },
+    {
+      "id": 3,
+      "name": "Jhone Duoe",
+      "email": "jhone.duoe@example.com",
+      "age": 23
+    },
+    {
+      "id": 4,
+      "name": "Jhone Duoe",
+      "email": "jhone.duoe@example.com",
+      "age": 23
+    },
+    {
+      "id": 5,
+      "name": "Jhone Duoe",
+      "email": "jhone.duoe@example.com",
+      "age": 20
+    }
+  ]
